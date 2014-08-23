cd5d58b7a924bc7f2c7396d072072a3b0f516108
thechris <chrismika93@gmail.com>
2014-08-23 01:52:54 -0500

    added tele beacon

---
+++ Source/Telefragger/TeleBeacon.h
@@ -0,0 +1,18 @@
+
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "TeleBeacon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TELEFRAGGER_API ATeleBeacon : public AActor
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
