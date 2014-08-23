5058ef4461c31a7912a294fbd5d0a766bef728bd
Ronald Kinard <ronkinard93@gmail.com>
2014-08-22 23:45:14 -0500

    Add main game framework classes.

---
+++ Source/Telefragger/DeathmatchGameState.cpp
@@ -0,0 +1,8 @@
+#include "Telefragger.h"
+#include "DeathmatchGameState.h"
+
+ADeathmatchGameState::ADeathmatchGameState(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
---
+++ Source/Telefragger/DeathmatchPlayerState.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "GameFramework/PlayerState.h"
+#include "DeathmatchPlayerState.generated.h"
+
+UCLASS()
+class TELEFRAGGER_API ADeathmatchPlayerState : public APlayerState
+{
+	GENERATED_UCLASS_BODY()
+};
