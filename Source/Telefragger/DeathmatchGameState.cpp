e547928a6037276a0c098f9717f9c4133ec47d13
Ronald Kinard <ronkinard93@gmail.com>
2014-08-25 13:45:43 -0500

    stupid sound

--- Source/Telefragger/DeathmatchGameState.cpp
+++ Source/Telefragger/DeathmatchGameState.cpp
@@ -4,8 +4,6 @@
 ADeathmatchGameState::ADeathmatchGameState(const class FPostConstructInitializeProperties& PCIP)
 	: Super(PCIP)
 {
-	static ConstructorHelpers::FObjectFinder<USoundWave> MessageSound(TEXT("SoundWave'/Game/Sounds/MessageBeep.MessageBeep'"));
-	MessageWave = MessageSound.Object;
 }
 
 void ADeathmatchGameState::PlayerJoinedServer_Implementation(const FString& PlayerName)
@@ -13,14 +11,6 @@ void ADeathmatchGameState::PlayerJoinedServer_Implementation(const FString& Play
 	if (GEngine)
 	{
 		GEngine->AddOnScreenDebugMessage(FMath::Rand(), 10.f, FColor::Cyan, FString::Printf(TEXT("%s has joined the server."), *PlayerName));
-		if (MessageWave && GWorld && GWorld->GetFirstLocalPlayerFromController())
-		{
-			APlayerController* Controller = GWorld->GetFirstLocalPlayerFromController()->PlayerController;
-			if (Controller != nullptr)
-			{
-				//Controller->ClientPlaySound(MessageWave, 1, 1);
-			}
-		}
 	}
 }
 
@@ -29,14 +19,6 @@ void ADeathmatchGameState::PlayerLeftServer_Implementation(const FString& Player
 	if (GEngine)
 	{
 		GEngine->AddOnScreenDebugMessage(FMath::Rand(), 10.f, FColor::Cyan, FString::Printf(TEXT("%s has left the server."), *PlayerName));
-		if (MessageWave && GWorld && GWorld->GetFirstLocalPlayerFromController())
-		{
-			APlayerController* Controller = GWorld->GetFirstLocalPlayerFromController()->PlayerController;
-			if (Controller != nullptr)
-			{
-				//Controller->ClientPlaySound(MessageWave, 1, 1);
-			}
-		}
 	}
 }
 
@@ -45,13 +27,5 @@ void ADeathmatchGameState::PlayerChat_Implementation(const FString& PlayerName, 
 	if (GEngine)
 	{
 		GEngine->AddOnScreenDebugMessage(FMath::Rand(), 10.f, FColor::White, FString::Printf(TEXT("%s: %s"), *PlayerName, *Message));
-		if (MessageWave && GWorld && GWorld->GetFirstLocalPlayerFromController())
-		{
-			APlayerController* Controller = GWorld->GetFirstLocalPlayerFromController()->PlayerController;
-			if (Controller != nullptr)
-			{
-				//Controller->ClientPlaySound(MessageWave, 1, 1);
-			}
-		}
 	}
 }
